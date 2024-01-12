06de36c831a73aea4945c40e9f0a768a75230acd
Leonardo Pedroso dos Santos <leonardopedroso27@yahoo.com.br>
2024-01-12 20:55:06 -0300

    Correções do codigo

---
+++ banda.py
@@ -0,0 +1,8 @@
+print("***Gerador de nome da banda***\n")
+
+cidade = input("Qual o nome da sua cidade?\n")
+pet = input("Qual o nome do seu animal?\n")
+
+print("\nO nome da sua banda é: " + cidade + " " + pet)
+
+print(input("\nClick ENTER to close the window"))
